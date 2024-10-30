e133a4b91db13eeae1c98d831252e6059b0a16b3
RINQERLAPTOP\tomoki <furukawa12122003@gmail.com>
2024-10-30 16:29:47 +0900

    リファクタ

--- main.py
+++ main.py
@@ -1,63 +1,45 @@
 from collections import Counter
+from AttributeIndex import AttributeIndex
 
-class AttributeIndex:
-    DURATION = 0
-    SERVICE = 1
-    SOURCE_BYTES = 2
-    DESTINATION_BYTES = 3
-    COUNT = 4
-    SAME_SRV_RATE = 5
-    SERROR_RATE = 6
-    SRV_SERROR_RATE = 7
-    DST_HOST_COUNT = 8
-    DST_HOST_SRV_COUNT = 9
-    DST_HOST_SAME_SRC_PORT_RATE = 10
-    DST_HOST_SERROR_RATE = 11
-    DST_HOST_SRV_SERROR_RATE = 12
-    FLAG = 13
-    IDS_DETECTION = 14
-    MALWARE_DETECTION = 15
-    ASHULA_DETECTION = 16
-    LABEL = 17
-    SOURCE_IP_ADDRESS = 18
-    SOURCE_PORT_NUMBER = 19
-    DESTINATION_IP_ADDRESS = 20
-    DESTINATION_PORT_NUMBER = 21
-    START_TIME = 22
-    DURATION_DUPLICATE = 23
 
-PATH =  r"C:\Users\tomoki\Downloads\201501\Kyoto2016\2015\01\20150105.txt"
+PATH = r"C:\Users\tomoki\Downloads\201501\Kyoto2016\2015\01\20150105.txt"
+FILLTERD_TRAFFIC_LOG_NAME = r"log\filtered_traffic.txt"
+ATTRIBUTE_DETAILS_LOG_NAME = r"log\attribute_details.txt"
 SEARCH_INDEX = AttributeIndex.IDS_DETECTION
 
-counter = Counter()
-target_values = set()
 
-def print_line(items, search_index):
-    print(items)
-    print(f"index {search_index} : {items[search_index]}")
+def process_file(input_path, log_path, count_path):
+    counter = Counter()
+    search_target_counter = Counter()
 
-def wait_input():
-    print("続行するには何かキーを押してください ...")
-    input()
+    with open(log_path, 'w') as log_file:
+        with open(input_path, 'r') as f:
+            for line in f:
+                counter["traffic_cnt"] += 1
+                items = line.split('\t')
 
-with open(PATH, 'r') as f:
-    for line in f:    
-        counter["line_cnt"] += 1
-        items = line.split('\t')
+                if items[SEARCH_INDEX] != "0":
+                    search_target = items[SEARCH_INDEX]
+                    log_file.write(line)
+                    search_target_counter[search_target] += 1
+                    counter["target_cnt"] += 1
 
-        if items[SEARCH_INDEX] != "0":
-            search_target = items[SEARCH_INDEX]
-            target_values.add(search_target)
-            counter["target_cnt"] += 1
+                    if items[AttributeIndex.LABEL] != "1":
+                        counter["attack_cnt"] += 1
 
-            # Labelが正常ではない場合にlabel_cntをインクリメント
-            if items[AttributeIndex.LABEL] != "1":
-                counter["label_cnt"] += 1
-        
-        # print_line(items, search_index)
-        # wait_input()
+    with open(count_path, 'w') as count_file:
+        for target, count in search_target_counter.items():
+            count_file.write(f"{target} : {count}\n")
 
-print("line count: ", counter["line_cnt"])
-print("target count: ", counter["target_cnt"])
-print("label count: ", counter["label_cnt"])
-print("dictionary: ", target_values)
+    return counter
+
+
+def main():
+    counter = process_file(PATH, FILLTERD_TRAFFIC_LOG_NAME, ATTRIBUTE_DETAILS_LOG_NAME)
+
+    for item, count in counter.items():
+        print(f"{item} : {count}")
+
+
+if __name__ == "__main__":
+    main()
