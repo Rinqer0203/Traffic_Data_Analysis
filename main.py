829b8fb78fdedef4b9457028154f28cefc97b610
RINQERDESKTOP\Tomoki <furukawa12122003@gmail.com>
2024-11-08 00:38:36 +0900

    logフォルダが存在しないときに生成、dataフォルダに解析対象のトラフィックデータを配置、リファクタ

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
+from os import makedirs
 from collections import Counter
 from traffic_attributes import TrafficAttributes
 
 
-PATH = r"C:\Users\tomoki\Downloads\201501\Kyoto2016\2015\01\20150105.txt"
-FILLTERD_TRAFFIC_LOG_NAME = r"log\filtered_traffic.txt"
-ATTRIBUTE_DETAILS_LOG_NAME = r"log\attribute_details.txt"
+DATA_PATH = r".\data\20150101.txt"
+FILLTERD_TRAFFIC_LOG_PATH = r".\log\filtered_traffic.txt"  # 検索で絞り込まれたログを保存するファイル
+ATTRIBUTE_DETAILS_LOG_PATH = r".\log\attribute_details.txt"  # 検索対象の詳細を保存するファイル
 SEARCH_INDEX = TrafficAttributes.MALWARE_DETECTION
 
 
-def process_file(input_path, log_path=FILLTERD_TRAFFIC_LOG_NAME, count_path=ATTRIBUTE_DETAILS_LOG_NAME):
+def process_file(input_path):
+    makedirs("./log", exist_ok=True)
     counter = Counter()
     search_target_counter = Counter()
 
-    with open(log_path, 'w') as log_file:
+    with open(FILLTERD_TRAFFIC_LOG_PATH, 'w') as fillterd_traffic_log:
         with open(input_path, 'r') as f:
             for line in f:
                 counter["traffic"] += 1
                 items = line.split('\t')
+                if items[TrafficAttributes.LABEL] != "1":
+                    counter["total_attack"] += 1
 
                 if items[SEARCH_INDEX] != "0":
+                    fillterd_traffic_log.write(line)
                     search_target = items[SEARCH_INDEX]
-                    log_file.write(line)
                     search_target_counter[search_target] += 1
                     counter["target"] += 1
 
                     if items[TrafficAttributes.LABEL] != "1":
                         counter["attack"] += 1
 
-    with open(count_path, 'w') as count_file:
+    with open(ATTRIBUTE_DETAILS_LOG_PATH, 'w') as attribute_details_log:
         for target, count in search_target_counter.items():
-            count_file.write(f"{target} : {count}\n")
+            attribute_details_log.write(f"{target} : {count}\n")
 
     return counter
 
 
 def main():
-    counter = process_file(PATH)
+    counter = process_file(DATA_PATH)
 
     for item, count in counter.items():
         print(f"{item} : {count}")
