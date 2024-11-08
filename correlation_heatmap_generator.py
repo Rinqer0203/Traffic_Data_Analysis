01019d256d1c80c81cf602b35599b6514e349d58
RINQERDESKTOP\Tomoki <furukawa12122003@gmail.com>
2024-11-08 10:40:10 +0900

    相関係数ヒートマップで3種類の相関を計算するように変更。

--- correlation_heatmap_generator.py
+++ correlation_heatmap_generator.py
@@ -5,7 +5,8 @@ import os
 import tsv_header_adder
 
 DATA_PATH = './data/20150101.txt'
-OUTPUT_IMG_PATH = './output/correlation_matrix.png'
+OUTPUT_DIR = './output/correlation_heatmap'
+os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 data = pd.read_csv(tsv_header_adder.get_tsv_with_header(DATA_PATH), delimiter='\t')
 
@@ -13,17 +14,22 @@ data = pd.read_csv(tsv_header_adder.get_tsv_with_header(DATA_PATH), delimiter='\
 selected_columns = ['DURATION', 'SOURCE_BYTES', 'DESTINATION_BYTES', 'COUNT', 'SAME_SRV_RATE', 'SERROR_RATE', 'SRV_SERROR_RATE', 'DST_HOST_COUNT',
                     'DST_HOST_SRV_COUNT', 'DST_HOST_SAME_SRC_PORT_RATE', 'DST_HOST_SERROR_RATE', 'DST_HOST_SRV_SERROR_RATE', 'SOURCE_PORT_NUMBER', 'LABEL']
 
-# 選択した列だけを取り出して相関を計算
+# 選択した列だけを取り出す
 selected_data = data[selected_columns]
-correlation_matrix = selected_data.corr()
 
-# ヒートマップ作成
-plt.title("Correlation Matrix", fontsize=16)
-plt.figure(figsize=(14, 14))
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=0.5)
-plt.tight_layout()
+# 3種類の相関方法を計算し、それぞれ保存
+correlation_methods = ['pearson', 'spearman', 'kendall']
+for method in correlation_methods:
+    # 相関行列の計算
+    correlation_matrix = selected_data.corr(method=method)
 
-# plt.show()
+    # ヒートマップ作成
+    plt.figure(figsize=(14, 14))
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=0.5)
+    plt.title(f"Correlation Matrix ({method.capitalize()})", fontsize=16)
+    plt.tight_layout()
 
-os.makedirs(os.path.dirname(OUTPUT_IMG_PATH), exist_ok=True)
-plt.savefig(OUTPUT_IMG_PATH)
+    # 画像保存
+    output_img_path = os.path.join(OUTPUT_DIR, f'correlation_matrix_{method}.png')
+    plt.savefig(output_img_path)
+    plt.close()
