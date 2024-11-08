c9fe44c61385d8244874f8b1d7ffb94a56c96a72
RINQERDESKTOP\Tomoki <furukawa12122003@gmail.com>
2024-11-09 03:49:50 +0900

    不要コード削除

--- utils/traffic_attributes.py
+++ utils/traffic_attributes.py
@@ -2,11 +2,6 @@ from typing import NamedTuple
 from enum import IntEnum
 
 
-class Attr(NamedTuple):
-    index: int
-    name: str
-
-
 class TrafficAttr(IntEnum):
     DURATION = 0
     SERVICE = 1
@@ -32,34 +27,6 @@ class TrafficAttr(IntEnum):
     DESTINATION_PORT_NUMBER = 21
     START_TIME = 22
     PROTOCOL = 23
-
-    # def get_attribute_list() -> list:
-    #     return [
-    #         Attr(TrafficAttr.DURATION, 'DURATION'),
-    #         Attr(TrafficAttr.SERVICE, 'SERVICE'),
-    #         Attr(TrafficAttr.SOURCE_BYTES, 'SOURCE_BYTES'),
-    #         Attr(TrafficAttr.DESTINATION_BYTES, 'DESTINATION_BYTES'),
-    #         Attr(TrafficAttr.COUNT, 'COUNT'),
-    #         Attr(TrafficAttr.SAME_SRV_RATE, 'SAME_SRV_RATE'),
-    #         Attr(TrafficAttr.SERROR_RATE, 'SERROR_RATE'),
-    #         Attr(TrafficAttr.SRV_SERROR_RATE, 'SRV_SERROR_RATE'),
-    #         Attr(TrafficAttr.DST_HOST_COUNT, 'DST_HOST_COUNT'),
-    #         Attr(TrafficAttr.DST_HOST_SRV_COUNT, 'DST_HOST_SRV_COUNT'),
-    #         Attr(TrafficAttr.DST_HOST_SAME_SRC_PORT_RATE, 'DST_HOST_SAME_SRC_PORT_RATE'),
-    #         Attr(TrafficAttr.DST_HOST_SERROR_RATE, 'DST_HOST_SERROR_RATE'),
-    #         Attr(TrafficAttr.DST_HOST_SRV_SERROR_RATE, 'DST_HOST_SRV_SERROR_RATE'),
-    #         Attr(TrafficAttr.FLAG, 'FLAG'),
-    #         Attr(TrafficAttr.IDS_DETECTION, 'IDS_DETECTION'),
-    #         Attr(TrafficAttr.MALWARE_DETECTION, 'MALWARE_DETECTION'),
-    #         Attr(TrafficAttr.ASHULA_DETECTION, 'ASHULA_DETECTION'),
-    #         Attr(TrafficAttr.LABEL, 'LABEL'),
-    #         Attr(TrafficAttr.SOURCE_IP_ADDRESS, 'SOURCE_IP_ADDRESS'),
-    #         Attr(TrafficAttr.SOURCE_PORT_NUMBER, 'SOURCE_PORT_NUMBER'),
-    #         Attr(TrafficAttr.DESTINATION_IP_ADDRESS, 'DESTINATION_IP_ADDRESS'),
-    #         Attr(TrafficAttr.DESTINATION_PORT_NUMBER, 'DESTINATION_PORT_NUMBER'),
-    #         Attr(TrafficAttr.START_TIME, 'START_TIME'),
-    #         Attr(TrafficAttr.PROTOCOL, 'PROTOCOL')
-    #     ]
 
     def get_attribute_name_list() -> list:
         return list(TrafficAttr.__members__.keys())
