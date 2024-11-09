01ea4c1f6e5c529033575b5f9bebb1796362ad1f
RINQERDESKTOP\Tomoki <furukawa12122003@gmail.com>
2024-11-10 02:54:06 +0900

    docstring追加。リファクタ

--- utils/tsv_header_adder.py
+++ utils/tsv_header_adder.py
@@ -2,25 +2,31 @@ import os
 from .traffic_attributes import TrafficAttr
 
 
-def get_tsv_with_header(input_file: str, output_dir: str | None = None, output_filename: str | None = None) -> str:
-    if output_dir is None:
-        output_dir = os.path.join(os.path.dirname(input_file), 'with_header')
-    if output_filename is None:
-        output_filename = os.path.basename(input_file)
-    output_file = os.path.join(output_dir, output_filename)
+def get_tsv_with_header(input_file: str, output_path: str | None = None) -> str:
+    '''
+    tsvファイルにヘッダーを追加して保存する
+    Args:
+        input_file (str): ヘッダーを追加する元のTSVファイルのパス。
+        output_path (str | None): 出力ファイルのパス。Noneの場合は、入力ファイルと同じディレクトリに 'with_header' ディレクトリを作成し、その中に保存。
+
+    Returns:
+        str: ヘッダーが追加された出力ファイルのパス。
+    '''
+    if output_path is None:
+        output_path = os.path.join(os.path.dirname(input_file), 'with_header', os.path.basename(input_file))
 
     # 既にヘッダーが付与されたファイルが存在する場合はそのファイルパスを返す
-    if os.path.exists(output_file):
-        return output_file
+    if os.path.exists(output_path):
+        return output_path
 
-    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
     header = TrafficAttr.get_attribute_name_list()
 
-    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+    with open(input_file, 'r') as infile, open(output_path, 'w') as outfile:
         outfile.write('\t'.join(header) + '\n')
         outfile.write(infile.read())
 
-    return output_file
+    return output_path
 
 
 def main():
